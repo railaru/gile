32a3f47e81579007dec10e3ca80f203bc4b34a2e
rutenisraila <rutenisraila@hotmail.com>
2023-07-11 13:13:45 +0200

    update textarea focus styles

--- src/components/ui/Textarea/Textarea.tsx
+++ src/components/ui/Textarea/Textarea.tsx
@@ -12,7 +12,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
         className={cn(
           "placeholder:text-muted-foreground resize-none",
           "flex min-h-[120px] w-full px-3 py-2",
-          "ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+          "ring-offset-background focus-visible:outline-none focus-visible:ring-2",
           "disabled:cursor-not-allowed disabled:opacity-50",
           "rounded-[4px] border border-input bg-white font-[300]",
           className
