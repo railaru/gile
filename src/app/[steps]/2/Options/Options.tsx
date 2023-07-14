11d4bac561f16d337593d0d088fd77a1a65135b9
rutenisraila <rutenisraila@hotmail.com>
2023-07-14 18:32:44 +0200

    add option validation

--- src/app/[steps]/2/Options/Options.tsx
+++ src/app/[steps]/2/Options/Options.tsx
@@ -5,10 +5,33 @@ import { v4 as uuidv4 } from "uuid";
 import Button from "@/components/ui/Button/Button";
 import useOptionsStore from "@/app/store/options";
 import { Option } from "@/types/options";
+import { PlusIcon } from "lucide-react";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Input from "@/components/ui/Input/Input";
+
+const schema = z.object({
+  title: z
+    .string()
+    .min(3, { message: "An option should be at least 3 character long." })
+    .max(60, {
+      message: "A deoptioncision should be at most 60 characters long.",
+    }),
+});
+
+type FormData = z.infer<typeof schema>;
 
 export default function Options() {
   const { options, setOptions, removeOption } = useOptionsStore();
   const [title, setTitle] = useState("");
+
+  const form = useForm<FormData>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      title,
+    },
+  });
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -51,17 +74,20 @@ export default function Options() {
       </ul>
 
       <form onSubmit={handleSubmit} className="flex flex-col mt-4 space-y-8">
-        <input
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          className="border border-neutral-6 rounded-[4px] p-4"
+        <Input
+          placeholder="Enter a new option"
+          {...form.register("title")}
+          className="h-[50px]"
         />
 
         <div className="h-[1px] bg-neutral-6" />
 
         <div>
-          <Button type="submit">Add option</Button>
+          <Button type="submit" disabled={!form.formState.isValid}>
+            <PlusIcon className="w-[18px] h-[18px] mr-1.5 relative bottom-[1px]" />
+
+            <span>Add Option</span>
+          </Button>
         </div>
       </form>
     </div>
---
+++ src/components/ui/Input/Input.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          "placeholder:font-[200] flex h-10 w-full ring-offset-background focus-visible:outline-none focus-visible:ring-2 rounded-[4px] border border-input bg-background px-3 py-2  file:border-0 file:bg-transparent file:font-medium placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+Input.displayName = "Input";
+
+export default Input;
