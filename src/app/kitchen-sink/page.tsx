0ba3d1b461ae86911639f999c140b56c4eb58965
rutenisraila <rutenisraila@hotmail.com>
2023-07-15 14:48:02 +0200

    add redirect to homepage to routes other than homepage

--- src/app/kitchen-sink/page.tsx
+++ src/app/kitchen-sink/page.tsx
@@ -2,8 +2,14 @@ import Pane from "@/components/ui/Pane/Pane";
 import React from "react";
 import ExampleForm from "./ExampleForm/ExampleForm";
 import { ProfileForm } from "./ProfileForm/ProfileForm";
+import { redirect } from "next/navigation";
+import { PAGE_ROUTES } from "@/constants/routes";
 
 export default function Page() {
+  if (process.env.NODE_ENV !== "development") {
+    redirect(PAGE_ROUTES.HOME);
+  }
+
   return (
     <div className="container mt-12 lg:mt-24">
       <h1 className="text-3xl font-medium">Kitchen sink</h1>
