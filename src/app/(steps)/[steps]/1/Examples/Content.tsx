e1e4011c3cb97cab766ba7115834bd0552b498b2
rutenisraila <rutenisraila@hotmail.com>
2023-08-19 11:15:01 +0200

    style mobile examples

--- src/app/(steps)/[steps]/1/Examples/Content.tsx
+++ src/app/(steps)/[steps]/1/Examples/Content.tsx
@@ -22,8 +22,8 @@ export default function Content() {
                 <ul>
                     {mockData.map((item, index) => (
                         <li key={index} className="mt-20 bg-neutral-7/50">
-                            <div className="p-8 mt-8">
-                                <h4 className="text-[70px] -mt-[80px]" aria-label="Emoji">
+                            <div className="p-4 lg:p-8 mt-4 lg:mt-8">
+                                <h4 className="text-[50px] -mt-[50px] lg:text-[70px] lg:-mt-[80px]" aria-label="Emoji">
                                     {item.emoji}
                                 </h4>
 
