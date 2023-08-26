92c2e65eec1a16fa5c0e99e08b94f9c4e10f6d0f
rutenisraila <rutenisraila@hotmail.com>
2023-08-26 11:24:02 +0200

    DAR-24: add fade in for UserButton

--- src/app/Components/DashboardHeader/DashboardHeader.tsx
+++ src/app/Components/DashboardHeader/DashboardHeader.tsx
@@ -1,15 +1,21 @@
+'use client';
+
 import React from 'react';
 import IconLogo from '../../../components/ui/Logo/IconLogo';
 import Link from 'next/link';
 import { PAGE_ROUTES } from '@/constants/routes';
 import HamburgerMenu from './HamburgerMenu';
 import { UserButton } from '@clerk/nextjs';
+import { cn } from '@/lib/utils';
+import { useConvexAuth } from 'convex/react';
 
 
 type Props = {
     showHamburgerMenu?: boolean;
 }
 export default function DashboardHeader({ showHamburgerMenu }: Props) {
+    const { isLoading } = useConvexAuth();
+
     return (
         <header
             className="px-4 lg:px-8 py-[10.5px] h-[54px] border-b border-neutral-6 flex items-center justify-between"
@@ -18,7 +24,14 @@ export default function DashboardHeader({ showHamburgerMenu }: Props) {
                 <IconLogo className="w-[28px] h-[28px] fill-black"/>
             </Link>
 
-            <UserButton afterSignOutUrl={PAGE_ROUTES.HOME}/>
+            <div
+                className={cn('transition', {
+                    'opacity-0 pointer-events-none': isLoading,
+                    'opacity-100 pointer-events-auto': !isLoading,
+                })}
+            >
+                <UserButton afterSignOutUrl={PAGE_ROUTES.HOME}/>
+            </div>
 
             {
                 showHamburgerMenu && <HamburgerMenu/>
--- src/app/Components/Header/Header.tsx
+++ src/app/Components/Header/Header.tsx
@@ -1,17 +1,30 @@
+'use client';
+
 import FullLogo from '@/components/ui/Logo/FullLogo';
 import Link from 'next/link';
 import { PAGE_ROUTES } from '@/constants/routes';
 import { UserButton } from '@clerk/nextjs';
 import React from 'react';
+import { useConvexAuth } from 'convex/react';
+import { cn } from '@/lib/utils';
 
 export default function Header() {
+    const { isLoading } = useConvexAuth();
+
     return (
         <header className="pt-4 lg:pt-8 flex items-center justify-between">
             <Link href={PAGE_ROUTES.HOME} className="inline-block">
                 <FullLogo className={'fill-black'}/>
             </Link>
 
-            <UserButton afterSignOutUrl={PAGE_ROUTES.HOME}/>
+            <div
+                className={cn('transition', {
+                    'opacity-0 pointer-events-none': isLoading,
+                    'opacity-100 pointer-events-auto': !isLoading,
+                })}
+            >
+                <UserButton afterSignOutUrl={PAGE_ROUTES.HOME}/>
+            </div>
         </header>
     );
 }
