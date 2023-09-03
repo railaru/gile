2cf13a918d5758e96d290e29fa6af4e96f87ca69
Rutenis Raila <rutenisraila@hotmail.com>
2023-09-03 16:10:04 +0200

    DAR-25: style clerk form

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -2,13 +2,13 @@ import { SignIn } from '@clerk/nextjs';
 import Header from '@/app/Components/Header/Header';
 
 export default function Page() {
-    return (
-        <div className="px-4 max-w-[1142px] mx-auto">
-            <Header/>
+	return (
+		<div className="px-4 max-w-[1142px] mx-auto space-y-6">
+			<Header/>
 
-            <div className="flex justify-center">
-                <SignIn/>
-            </div>
-        </div>
-    );
+			<div className="flex justify-center">
+				<SignIn/>
+			</div>
+		</div>
+	);
 }
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -2,13 +2,13 @@ import { SignUp } from '@clerk/nextjs';
 import Header from '@/app/Components/Header/Header';
 
 export default function Page() {
-    return (
-        <div className="px-4 max-w-[1142px] mx-auto">
-            <Header/>
+	return (
+		<div className="px-4 max-w-[1142px] mx-auto space-y-6">
+			<Header/>
 
-            <div className="flex justify-center">
-                <SignUp/>
-            </div>
-        </div>
-    );
+			<div className="flex justify-center">
+				<SignUp/>
+			</div>
+		</div>
+	);
 }
