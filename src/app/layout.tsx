a496e7787719e23693e89a04f75db68c018bb0e0
Rutenis Raila <rutenisraila@hotmail.com>
2023-09-03 15:39:00 +0200

    DAR-25: style clerk form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,75 +8,75 @@ import { ClerkProvider } from '@clerk/nextjs';
 import ConvexClientProvider from '@/app/Providers/ConvexClientProvider';
 
 const matter = localFont({
-    variable: '--font-matter',
-    src: [
-        {
-            path: './../../public/assets/fonts/Matter-Light.ttf',
-            weight: '200',
-        },
-        {
-            path: './../../public/assets/fonts/Matter-LightItalic.ttf',
-            weight: '200',
-            style: 'italic',
-        },
-        {
-            path: './../../public/assets/fonts/Matter-Regular.ttf',
-            weight: '300',
-            style: 'normal',
-        },
-        {
-            path: './../../public/assets/fonts/Matter-RegularItalic.ttf',
-            weight: '300',
-            style: 'italic',
-        },
-        {
-            path: './../../public/assets/fonts/Matter-Medium.ttf',
-            weight: '400',
-        },
-        {
-            path: './../../public/assets/fonts/Matter-MediumItalic.ttf',
-            weight: '400',
-            style: 'italic',
-        },
-        {
-            path: './../../public/assets/fonts/Matter-Bold.ttf',
-            weight: '500',
-        },
-        {
-            path: './../../public/assets/fonts/Matter-BoldItalic.ttf',
-            weight: '500',
-            style: 'italic',
-        },
-        {
-            path: './../../public/assets/fonts/Matter-Heavy.ttf',
-            weight: '600',
-            style: 'normal',
-        },
-        {
-            path: './../../public/assets/fonts/Matter-HeavyItalic.ttf',
-            weight: '600',
-            style: 'italic',
-        },
-    ],
+	variable: '--font-matter',
+	src: [
+		{
+			path: './../../public/assets/fonts/Matter-Light.ttf',
+			weight: '200',
+		},
+		{
+			path: './../../public/assets/fonts/Matter-LightItalic.ttf',
+			weight: '200',
+			style: 'italic',
+		},
+		{
+			path: './../../public/assets/fonts/Matter-Regular.ttf',
+			weight: '300',
+			style: 'normal',
+		},
+		{
+			path: './../../public/assets/fonts/Matter-RegularItalic.ttf',
+			weight: '300',
+			style: 'italic',
+		},
+		{
+			path: './../../public/assets/fonts/Matter-Medium.ttf',
+			weight: '400',
+		},
+		{
+			path: './../../public/assets/fonts/Matter-MediumItalic.ttf',
+			weight: '400',
+			style: 'italic',
+		},
+		{
+			path: './../../public/assets/fonts/Matter-Bold.ttf',
+			weight: '500',
+		},
+		{
+			path: './../../public/assets/fonts/Matter-BoldItalic.ttf',
+			weight: '500',
+			style: 'italic',
+		},
+		{
+			path: './../../public/assets/fonts/Matter-Heavy.ttf',
+			weight: '600',
+			style: 'normal',
+		},
+		{
+			path: './../../public/assets/fonts/Matter-HeavyItalic.ttf',
+			weight: '600',
+			style: 'italic',
+		},
+	],
 });
 
 export const metadata: Metadata = {
-    title: 'Gile',
-    description: 'Make Better Decisions',
+	title: 'Gile',
+	description: 'Make Better Decisions',
 };
 
-export default function RootLayout({ children }: { children: ReactNode }) {
-    return (
-        <ClerkProvider>
-            <ConvexClientProvider>
-                <html lang="en">
-                <body className={matter.variable}>
-                <RouteChangeAnimation/>
+export default function RootLayout({children}: { children: ReactNode }) {
+	return (
+		<ClerkProvider>
+			<ConvexClientProvider>
+				<html lang="en">
+				<body className={matter.variable}>
+				<RouteChangeAnimation/>
 
-                {children}
-                </body>
-                </html>
-            </ConvexClientProvider>
-        </ClerkProvider>
-    );
+				{children}
+				</body>
+				</html>
+			</ConvexClientProvider>
+		</ClerkProvider>
+	);
 }
