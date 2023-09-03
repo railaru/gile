5aaa07499493ee0ec41aa79bd60cb623532c04ce
Rutenis Raila <rutenisraila@hotmail.com>
2023-09-03 16:26:26 +0200

    DAR-25: add filtering by user id in decisions query

--- convex/decisions.ts
+++ convex/decisions.ts
@@ -2,85 +2,90 @@ import { mutation, query } from './_generated/server';
 import { v } from 'convex/values';
 
 export const get = query({
-    handler: async (ctx) => {
-        const identity = await ctx.auth.getUserIdentity();
+	handler: async (ctx) => {
+		const identity = await ctx.auth.getUserIdentity();
 
-        if (identity === null) {
-            return null;
-        }
+		if (identity === null) {
+			return null;
+		}
 
-        return await ctx.db.query('decisions')
-        .filter((q) => q.eq(q.field('userTokenIdentifier'), identity.tokenIdentifier))
-        .collect();
-    },
+		return await ctx.db.query('decisions')
+			.filter((q) => q.eq(q.field('userTokenIdentifier'), identity.tokenIdentifier))
+			.collect();
+	},
 });
 
 export const getById = query({
-    args: {
-        _id: v.string(),
-    },
-    handler: async (ctx, args) => {
-        if ( !args._id) {
-            return null;
-        }
+	args: {
+		_id: v.string(),
+	},
+	handler: async (ctx, args) => {
+		if (!args._id) {
+			return null;
+		}
 
-        const identity = await ctx.auth.getUserIdentity();
+		const identity = await ctx.auth.getUserIdentity();
 
-        if (identity === null) {
-            return null;
-        }
+		if (identity === null) {
+			return null;
+		}
 
-        return await ctx.db.query('decisions').filter((q) => q.eq(q.field('_id'), args._id)).first();
-    }
+		return await
+			ctx.db
+				.query('decisions')
+				.filter((q) => q.eq(q.field('_id'), args._id))
+				.filter((q) => q.eq(q.field('userTokenIdentifier'), identity.tokenIdentifier))
+				.first();
+	}
 });
 
 export const add = mutation({
-    args: {
-        decision: v.string(),
-    },
-    handler: async (ctx, args) => {
-        const identity = await ctx.auth.getUserIdentity();
+	args: {
+		decision: v.string(),
+	},
+	handler: async (ctx, args) => {
+		const identity = await ctx.auth.getUserIdentity();
 
-        if (identity === null) {
-            throw new Error('Called addDecision without authentication present');
-        }
+		if (identity === null) {
+			throw new Error('Called addDecision without authentication present');
+		}
 
-        return await ctx.db.insert('decisions', {
-            decision: args.decision,
-            userTokenIdentifier: identity.tokenIdentifier,
-        });
-    }
+		return await ctx.db.insert('decisions', {
+			decision: args.decision,
+			userTokenIdentifier: identity.tokenIdentifier,
+		});
+	}
 });
 
 export const edit = mutation({
-    args: {
-        _id: v.id('decisions'),
-        decision: v.string(),
-    },
-    handler: async (ctx, args) => {
-        const identity = await ctx.auth.getUserIdentity();
+	args: {
+		_id: v.id('decisions'),
+		decision: v.string(),
+	},
+	handler: async (ctx, args) => {
+		const identity = await ctx.auth.getUserIdentity();
 
-        if (identity === null) {
-            throw new Error('Called editDecision without authentication present');
-        }
+		if (identity === null) {
+			throw new Error('Called editDecision without authentication present');
+		}
 
-        return await ctx.db.patch(args._id, {
-            decision: args.decision,
-        });
-    }
+		return await ctx.db.patch(args._id, {
+			decision: args.decision,
+		});
+	}
 });
 
 export const deleteById = mutation({
-    args: {
-        _id: v.id('decisions'),
-    },
-    handler: async (ctx, args) => {
-        const identity = await ctx.auth.getUserIdentity();
+	args: {
+		_id: v.id('decisions'),
+	},
+	handler: async (ctx, args) => {
+		const identity = await ctx.auth.getUserIdentity();
 
-        if (identity === null) {
-            throw new Error('Called deleteDecision without authentication present');
-        }
+		if (identity === null) {
+			throw new Error('Called deleteDecision without authentication present');
+		}
 
-        return await ctx.db.delete(args._id);
-    }
+		return await ctx.db.delete(args._id);
+	}
 });
--- convex/schema.ts
+++ convex/schema.ts
@@ -3,22 +3,25 @@ import { v } from 'convex/values';
 
 
 export default defineSchema({
-    decisions: defineTable({ decision: v.string(), userTokenIdentifier: v.string(), }),
-    options: defineTable({
-        decisionId: v.id('decisions'),
-        userTokenIdentifier: v.string(),
-        title: v.string(),
-        ratings: v.object({
-            financialCost: v.number(),
-            levelOfEffort: v.number(),
-            timeInvestment: v.number(),
-            risk: v.number(),
-            shortTermReturn: v.number(),
-            longTermReturn: v.number(),
-        })
-    }).index('by_decision_id_and_user_token', ['decisionId', 'userTokenIdentifier']),
-    users: defineTable({
-        name: v.string(),
-        tokenIdentifier: v.string(),
-    }).index('by_token', ['tokenIdentifier']),
+	decisions: defineTable({
+		decision: v.string(),
+		userTokenIdentifier: v.string(),
+	}),
+	options: defineTable({
+		decisionId: v.id('decisions'),
+		userTokenIdentifier: v.string(),
+		title: v.string(),
+		ratings: v.object({
+			financialCost: v.number(),
+			levelOfEffort: v.number(),
+			timeInvestment: v.number(),
+			risk: v.number(),
+			shortTermReturn: v.number(),
+			longTermReturn: v.number(),
+		})
+	}).index('by_decision_id_and_user_token', ['decisionId', 'userTokenIdentifier']),
+	users: defineTable({
+		name: v.string(),
+		tokenIdentifier: v.string(),
+	}).index('by_token', ['tokenIdentifier']),
 });
