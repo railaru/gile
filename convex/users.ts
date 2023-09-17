c1b22e50a2505c17ea83fbe9e3c288cafcaab9d2
rutenisraila <rutenisraila@hotmail.com>
2023-09-17 19:00:42 +0200

    DAR-25: remove comments for storeUser mutation

--- convex/users.ts
+++ convex/users.ts
@@ -21,7 +21,6 @@ export const store = mutation({
             throw new Error('Called storeUser without authentication present');
         }
 
-        // Check if we've already stored this identity before.
         const user = await ctx.db
         .query('users')
         .withIndex('by_token', (q) =>
@@ -30,14 +29,12 @@ export const store = mutation({
         .unique();
 
         if (user !== null) {
-            // If we've seen this identity before but the name has changed, patch the value.
             if (user.name !== identity.name) {
                 await ctx.db.patch(user._id, { name: identity.name });
             }
             return user._id;
         }
 
-        // If it's a create identity, create a create `User`.
         return await ctx.db.insert('users', {
             name: identity.name!,
             tokenIdentifier: identity.tokenIdentifier,
