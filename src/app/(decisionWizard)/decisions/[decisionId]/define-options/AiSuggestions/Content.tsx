f26e6ccaa3884e1ebb37bb82296a4e30ecf837ee
Rūtenis Raila <rutenisraila@hotmail.com>
2024-06-24 16:16:14 +0300

    use structured data and openai functions

--- src/app/(decisionWizard)/decisions/[decisionId]/define-options/AiSuggestions/Content.tsx
+++ src/app/(decisionWizard)/decisions/[decisionId]/define-options/AiSuggestions/Content.tsx
@@ -1,136 +1,151 @@
-'use client';
+"use client";
 
-import React, { useEffect, useState } from 'react';
-import { SheetContent, SheetHeader, SheetTitle, } from '@/components/ui/Sheet/Sheet';
-import { useParams } from 'next/navigation';
-import { Id } from '../../../../../../../convex/_generated/dataModel';
-import { useMutation, useQuery } from 'convex/react';
-import { api } from '../../../../../../../convex/_generated/api';
-import { API_ROUTES } from '@/constants/routes';
-import Button from '@/components/ui/Button/Button';
-import { useToast } from '@/components/ui/Toaster/useToast';
-import { useOptions } from '@/hooks/queries/useOptions';
-import AiOptionSkeletons
-    from '@/app/(decisionWizard)/decisions/[decisionId]/define-options/AiSuggestions/AiOptionSkeletons';
+import React, { useEffect, useState } from "react";
+import {
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/Sheet/Sheet";
+import { useParams } from "next/navigation";
+import { Id } from "../../../../../../../convex/_generated/dataModel";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../../../../../../convex/_generated/api";
+import { API_ROUTES } from "@/constants/routes";
+import Button from "@/components/ui/Button/Button";
+import { useToast } from "@/components/ui/Toaster/useToast";
+import { useOptions } from "@/hooks/queries/useOptions";
+import AiOptionSkeletons from "@/app/(decisionWizard)/decisions/[decisionId]/define-options/AiSuggestions/AiOptionSkeletons";
 
 type Params = {
-    decision?: string;
-    userInterests?: string[];
-    userDescription?: string;
-}
+  decision?: string;
+  userInterests?: string[];
+  userDescription?: string;
+};
 
 type Data = {
-    message: {
-        content: string;
-    }
-}
+  message: {
+    content: string;
+  };
+};
 
 type Response = Data[];
 
+type DecisionOption = {
+  name: string;
+};
+
+type DataParsed = {
+  decision_options: DecisionOption[];
+};
+
 function useData({ decision, userInterests, userDescription }: Params) {
-    const formattedUserInterests = userInterests?.join(',');
-    const url = `${API_ROUTES.AI_OPTION_SUGGEST}?decision=${decision}&userInterests=${formattedUserInterests}&userDescription=${userDescription}`;
-    const [data, setData] = useState<Response | null>(null);
+  const formattedUserInterests = userInterests?.join(",");
+  const url = `${API_ROUTES.AI_OPTION_SUGGEST}?decision=${decision}&userInterests=${formattedUserInterests}&userDescription=${userDescription}`;
+  const [data, setData] = useState<Response | null>(null);
 
-    useEffect(() => {
-        if ( !decision || !userInterests || !userDescription) return;
+  useEffect(() => {
+    if (!decision || !userInterests || !userDescription) return;
 
-        fetch(url)
-        .then(response => response.json())
-        .then(data => setData(data as Response));
-    }, [decision, userInterests, userDescription]);
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setData(data as Response));
+  }, [decision, userInterests, userDescription]);
 
-    const isLoading = !data;
-    const content = data?.[0]?.message?.content || '';
-    const formattedData = content.split(',');
+  const dataParsed = data
+    ? (JSON.parse(data?.toString() || "") as DataParsed | undefined)
+    : undefined;
 
-    return {
-        isLoading,
-        data,
-        formattedData,
-    };
+  const isLoading = !data;
+
+  return {
+    isLoading,
+    data,
+    dataParsed,
+  };
 }
 
 export default function Content() {
-    const params = useParams();
-    const { toast } = useToast();
+  const params = useParams();
+  const { toast } = useToast();
 
-    const decisionId = params?.decisionId as Id<'decisions'>;
-    const storedDecisionRecord = useQuery(api.decisions.getById, { _id: decisionId });
-    const userData = useQuery(api.users.get);
-    const { data: options } = useOptions();
-    const addOption = useMutation(api.options.add);
-    const { formattedData, isLoading } = useData({
-        decision: storedDecisionRecord?.decision,
-        userInterests: userData?.interests,
-        userDescription: userData?.description,
-    });
-    const notYetAddedOptions = formattedData?.filter(item => {
-        return !options?.some(option => option.title === item);
-    });
+  const decisionId = params?.decisionId as Id<"decisions">;
+  const storedDecisionRecord = useQuery(api.decisions.getById, {
+    _id: decisionId,
+  });
+  const userData = useQuery(api.users.get);
+  const { data: options } = useOptions();
+  const addOption = useMutation(api.options.add);
+  const { isLoading, dataParsed } = useData({
+    decision: storedDecisionRecord?.decision,
+    userInterests: userData?.interests,
+    userDescription: userData?.description,
+  });
+  const notYetAddedOptions = dataParsed?.decision_options?.filter((item) => {
+    return !options?.some((option) => option.title === item.name);
+  });
 
-    const handleAdd = (title: string) => {
-        addOption({
-            decisionId,
-            title,
-            ratings: {
-                financialCost: 0,
-                levelOfEffort: 0,
-                timeInvestment: 0,
-                risk: 0,
-                shortTermReturn: 0,
-                longTermReturn: 0,
-            }
-        }).catch(() => {
-            toast({
-                title: 'Error',
-                description: 'Could not add option. Try coping it manually.',
-                variant: 'destructive',
-            });
-        }).then(() => {
-            toast({
-                title: 'Success',
-                description: 'Option added successfully.',
-            });
+  const handleAdd = (title: string) => {
+    addOption({
+      decisionId,
+      title,
+      ratings: {
+        financialCost: 0,
+        levelOfEffort: 0,
+        timeInvestment: 0,
+        risk: 0,
+        shortTermReturn: 0,
+        longTermReturn: 0,
+      },
+    })
+      .catch(() => {
+        toast({
+          title: "Error",
+          description: "Could not add option. Try coping it manually.",
+          variant: "destructive",
         });
-    };
+      })
+      .then(() => {
+        toast({
+          title: "Success",
+          description: "Option added successfully.",
+        });
+      });
+  };
 
-    return (
-        <SheetContent className="w-full sm:min-w-[540px] lg:min-w-[680px]">
-            <SheetHeader className="mt-10">
-                <SheetTitle>AI Suggestions</SheetTitle>
-            </SheetHeader>
+  return (
+    <SheetContent className="w-full sm:min-w-[540px] lg:min-w-[680px]">
+      <SheetHeader className="mt-10">
+        <SheetTitle>AI Suggestions</SheetTitle>
+      </SheetHeader>
 
-            <p className="text-neutral-2 mt-2">
-                AI Suggestions are generated based on the data you have provided.
-            </p>
+      <p className="mt-2 text-neutral-2">
+        AI Suggestions are generated based on the data you have provided.
+      </p>
 
-            <div className="mt-10">
-                {isLoading && (
-                    <AiOptionSkeletons/>
-                )}
+      <div className="mt-10">
+        {isLoading && <AiOptionSkeletons />}
 
-                { !isLoading && notYetAddedOptions && notYetAddedOptions.length > 0 && (
-                    <ul className="flex flex-col gap-4">
-                        {notYetAddedOptions.map((item, index) => (
-                            <li
-                                key={index}
-                                className="bg-neutral-7/50 rounded-[4px] px-4 py-[21.5px] flex justify-between items-center gap-4"
-                            >
-                                <p>{item}</p>
+        {!isLoading && notYetAddedOptions && notYetAddedOptions.length > 0 && (
+          <ul className="flex flex-col gap-4">
+            {notYetAddedOptions.map((item, index) => (
+              <li
+                key={index}
+                className="bg-neutral-7/50 rounded-[4px] px-4 py-[21.5px] flex justify-between items-center gap-4"
+              >
+                <p>{item.name}</p>
 
-                                <Button
-                                    variant="ghost"
-                                    type="button"
-                                    onClick={() => handleAdd(item)}
-                                >
-                                    Add
-                                </Button>
-                            </li>
-                        ))}
-                    </ul>
-                )}
-            </div>
-        </SheetContent>
-    );
+                <Button
+                  variant="ghost"
+                  type="button"
+                  onClick={() => handleAdd(item.name)}
+                >
+                  Add
+                </Button>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </SheetContent>
+  );
 }
--- src/app/api/ai-option-suggest/route.ts
+++ src/app/api/ai-option-suggest/route.ts
@@ -1,36 +1,61 @@
-import OpenAI from 'openai';
+import OpenAI from "openai";
 
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
+const schema = {
+  type: "object",
+  properties: {
+    decision_options: {
+      type: "array",
+      description: "Get a liist of decision options to choose from.",
+      items: {
+        type: "object",
+        properties: {
+          name: { type: "string", description: "A decision option name" },
+        },
+      },
+    },
+  },
+  required: ["decision_options"],
+};
+
 export async function GET(request: Request) {
-    const { searchParams } = new URL(request.url);
+  const { searchParams } = new URL(request.url);
 
-    const decision = searchParams.get('decision');
-    const description = searchParams.get('userDescription');
-    const interests = searchParams.get('userInterests');
-    const optionCharacterLimit = searchParams.get('optionCharacterLimit') || 60;
+  const decision = searchParams.get("decision");
+  const description = searchParams.get("userDescription");
+  const interests = searchParams.get("userInterests");
+  const optionCharacterLimit = searchParams.get("optionCharacterLimit") || 60;
 
-    const content = `
+  const content = `
         I am a highly motivated individual who is looking to make this decision: ${decision}. 
         I am interested in ${interests}.
         A short description about me: ${description}
         Give me a list of options I can choose from to make this decision.    
         Just state the options no need for intro.        
         Max length for one option is ${optionCharacterLimit} characters.
-        The format should be a long sentence separate by commas. For example: option 1, option 2, option 3.
-        No need to add numbered points. The format should be: wash dishes, take out trash, walk a dog.
-        Avoid the following format:
-        1. Pursue further education in a specialized field.\\n2. Explore opportunities for promotion within your current company.\\n3.       
     `;
 
-    const completion = await openai.chat.completions.create({
-        messages: [{ role: 'user', content }],
-        model: 'gpt-3.5-turbo',
-    });
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      { role: "system", content: "You are an expert advice giver." },
+      {
+        role: "user",
+        content,
+      },
+    ],
+    functions: [{ name: "get_decision_options_data", parameters: schema }],
+    function_call: { name: "get_decision_options_data" },
+    temperature: 0,
+  });
 
-    return new Response(JSON.stringify(completion.choices), {
-        status: 200,
-    });
+  return new Response(
+    JSON.stringify(response.choices[0].message.function_call?.arguments),
+    {
+      status: 200,
+    }
+  );
 }
